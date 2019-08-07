e56bccd5029b4a359918d8b91b7ac2a9b15256c5
Ashok Gudivada <ashok.gudivada@eagles.oc.edu>
2019-08-07 14:00:45 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ let secretNumber = 0,
  numberOfGuesses = 0,
     userGuessedNumber = 0;
 
-let writeMessage = (element, message, appendMessage) => {
-    let elementToUpdate = document.getElementById(element);
+let writeMessage = (element, message, appendMessage = false) => {
     if(element && message){
-        elementToUpdate.innerHTML = appendMessage ? elementToUpdate.innerHTML + message : message;
+        element.innerHTML = appendMessage ? element.innerHTML + message : message;
     }
 };
 
@@ -13,6 +12,7 @@ let writeMessage = (element, message, appendMessage) => {
 
 function newGame() {
     secretNumber = Math.floor(Math.random() * 100) + 1;
+    console.log("secretNumber "+ secretNumber);
     numberOfGuesses = 0;
     writeMessage(historyList, '');
     writeMessage()
@@ -23,37 +23,37 @@ function userGuessed(event) {
     const userGuessed = document.getElementById('userGuess');
     const statusArea = document.getElementById('statusArea');
     const historyList = document.getElementById('historyList');
-    userGuessed.value ? userGuessed.value : 0;
-    if (userGuessedNumber.length == 0 || (userGuessedNumber > 0 && userGuessedNumber < 101)){
+    userNumber = userGuessed.value ? parseInt(userGuessed.value) : 0;
+    if (Number.isNaN(userNumber) || (userNumber < 0 || userNumber > 100)){
         writeMessage (statusArea, '<p>Please guess a number between 1-100</p>');
-    } else if (userGuessedNumber.indexOf('.') != -1) {
+    } else if (!Number.isInteger(userNumber)) {
         writeMessage(statusArea, '<p>Please enter a whole number 1-100 ......' +
             ' secretNumber is whole</p>');
     } else {
         numberOfGuesses++;
-        if (userGuessed == secretNumber) {
+        if (userNumber == secretNumber) {
             // Got it
             writeMessage(statusArea, '<p>You got me in ' + numberOfGuesses +' guesses, I was thinking '
                 + secretNumber + '. </p>');
             if (confirm('Lets play again')) {
                 newGame();
             }
-        } else if (userGuessed < secretNumber) {
+        } else if (userNumber < secretNumber) {
             // User needs to guess higher
-            writeMessage(statusArea, '<p>You need to guess higher than ' + userGuessed + ', try again...</p>');
-            writeMessage(historyList, '<li>' + userGuessed +' (too low)</li>', true);
-            // TODO find the nth child in ol which equal to userGuessedNumber
+            writeMessage(statusArea, '<p>You need to guess higher than ' + userNumber + ', try again...</p>');
+            writeMessage(historyList, '<li>' + userNumber +' (too low)</li>', true);
+            // TODO find the nth child in ol which equal to userNumber
         } else {
             // User needs to guess lower
-            writeMessage(statusArea, '<p>You need to guess lower than ' + userGuessed + ', try again...</p>');
-            writeMessage(historyList, '<li>' + userGuessed + ' (too high)</li>', true);
-            // TODO find the nth child in ol which equal to userGuessedNumber
+            writeMessage(statusArea, '<p>You need to guess lower than ' + userNumber + ', try again...</p>');
+            writeMessage(historyList, '<li>' + userNumber + ' (too high)</li>', true);
+            // TODO find the nth child in ol which equal to userNumber
         }
     }
-    document.getElementById(userGuessed).value = '';
+    userGuessed.value = '';
 }
 
 window.onload = function() {
     newGame();
-    document.getElementById('form').onsubmit = userGuessed(event);
+    document.getElementById('form').onsubmit = userGuessed;
 };
